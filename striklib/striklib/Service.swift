dfcaf705d5c2db1bf2844cf58e59a7be1061b014
GKumar <gourav.kumar@essentialdesigns.net>
2019-05-30 10:37:40 +0530

    My First Commit for strik framework

---
+++ striklib/striklib/Service.swift
@@ -0,0 +1,22 @@
+//
+//  Service.swift
+//  striklib
+//
+//  Created by Admin on 30/05/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    
+    public static func dosomethind() -> String {
+        
+        return "Did some stuff, here you go home!"
+    }
+    
+}
---
+++ striklib/striklib/striklib.h
@@ -0,0 +1,19 @@
+//
+//  striklib.h
+//  striklib
+//
+//  Created by Admin on 30/05/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for striklib.
+FOUNDATION_EXPORT double striklibVersionNumber;
+
+//! Project version string for striklib.
+FOUNDATION_EXPORT const unsigned char striklibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <striklib/PublicHeader.h>
+
+
